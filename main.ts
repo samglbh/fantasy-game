ec6e95e1d243d900e2f9898ffab16da583ddf994
samglbh <155912005+samglbh@users.noreply.github.com>
2024-10-21 19:17:41 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function stat_screen () {
+    scene.setBackgroundColor(13)
+    tiles.setCurrentTilemap(tilemap`level2`)
+}
 function randomise_weapon (staff: boolean, img2: Image) {
     if (staff == true) {
         if (randint(0, 10) <= 5) {
@@ -34,7 +38,45 @@ function randomise_weapon (staff: boolean, img2: Image) {
                 `
         }
     } else {
-    	
+        if (randint(0, 10) <= 5) {
+            img2 = img`
+                . . . . . . . . . . . . . f f f 
+                . . . . . . . . . . . . f b b f 
+                . . . . . . . . . . . f b b c f 
+                . . . . . . . . . . f b b c f . 
+                . . . . . . . . . f b b c f . . 
+                . . . . . . . . f b b c f . . . 
+                . . f . . . . f b b c f . . . . 
+                . f 5 f . . f b b c f . . . . . 
+                . f 1 5 f f b b c f . . . . . . 
+                . . f 5 5 f b c f . . . . . . . 
+                . . . f f 2 f f . . . . . . . . 
+                . . f b b f 5 f . . . . . . . . 
+                . f e f c f 5 5 f . . . . . . . 
+                f e e e f . f 5 5 f . . . . . . 
+                f e e f . . . f f . . . . . . . 
+                f f f . . . . . . . . . . . . . 
+                `
+        } else {
+            img2 = img`
+                . . . . . . . . . . . . . f f f 
+                . . . . . . . . . . . . f b b f 
+                . 7 7 7 . . . 7 7 . . f b b c f 
+                . . . 7 . 7 7 . 7 . f b b c f . 
+                . . . . . . . . . f b b c f . . 
+                . . . 7 7 . . . f b b c f . . . 
+                . . f . 7 . . f b b c f . . . . 
+                . f 5 f . . f b b c f . . . . . 
+                . f 1 5 f f b b c f . . . 7 7 . 
+                . . f 5 5 f b c f . . . . . 7 . 
+                . . . f f 2 f f . . . . . . 7 . 
+                . . f b b f 5 f . 7 . 7 7 7 7 . 
+                . f e f c f 5 5 f . 7 . 7 7 . . 
+                f e e e f . f 5 5 f 7 7 . . . . 
+                f e e f . . . f f . . 7 7 . . . 
+                f f f . . . . . . . . . . . . . 
+                `
+        }
     }
 }
 let img2: Image = null
---
+++ tilemap.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000500000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
